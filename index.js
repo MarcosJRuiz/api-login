5c7b75e69ef6432162c5a418a41dc22dfe17ff29
MarcosJRuiz <marcosruiz.jt@gmail.com>
2023-04-27 22:32:17 -0300

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+﻿const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.status(200).send(`Hello World! the is my first api with port: ${port}`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
